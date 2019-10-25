bc972ad18267a5b9587bbfbb08790df4cbd0a452
pqs <pengqiangsheng@leelen.cn>
2019-10-25 14:31:58 +0800

    更新 pwa完整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,15 @@ module.exports = {
 	// 	lineNumbers: true
 	// },
 	head: [
+		['link', { rel: 'icon', href: '/logo.png' }],
 		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'theme-color', content: '#53A8FF' }],
+		['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+		['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+		['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+		['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#53A8FF' }],
+		['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+		['meta', { name: 'msapplication-TileColor', content: '#000000' }]
 	],
 	themeConfig: {
 		nav: [
